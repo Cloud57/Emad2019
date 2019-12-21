13c7f64c15bae690e7d4337da793f26fd11ed9aa
Cloud57 <andrea-palladino@hotmail.it>
2019-12-21 17:44:44 +0100

    http call

--- src/app/service/ruby-api.service.ts
+++ src/app/service/ruby-api.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
 import { EnvService } from './env.service';
 
 @Injectable({
@@ -10,7 +10,15 @@ export class RubyApiService {
   constructor(public http: HttpClient, public env:EnvService ) { }
 
   getUsers() {
-    return this.http.get(this.env.API_URL+"/users.json");
-  }
+    const params = new HttpParams();
+    params.set('email', "usertwo@gmail.com");
+    params.set('password', "123456");
+    let header = new HttpHeaders();
+    header.append('Content-Type', 'application/x-www-form-urlencoded');
+    header.append('Access-Control-Allow-Origin', '*')
+
+    return this.http.post(this.env.API_URL+"/authenticate",params, {
+      headers: header});
+    }
 
 }
