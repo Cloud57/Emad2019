e7a504039e3dda1f8d04f1b0c0c42617dead2096
Cloud57 <andrea-palladino@hotmail.it>
2020-01-10 10:06:05 +0100

    fix

--- src/app/genericPackage/page/paziente/lista-task/lista-task.page.ts
+++ src/app/genericPackage/page/paziente/lista-task/lista-task.page.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { NavController } from '@ionic/angular';
+import { GlobalService } from 'src/app/service/global.service';
+import { RubyApiService } from 'src/app/service/ruby-api.service';
 
 @Component({
   selector: 'app-lista-task',
@@ -7,8 +9,8 @@ import { NavController } from '@ionic/angular';
   styleUrls: ['./lista-task.page.scss'],
 })
 export class ListaTaskPage implements OnInit {
-
-  constructor(private navCtrl: NavController) { 
+  public response : any = [];
+  constructor(private navCtrl: NavController, private globalService: GlobalService, private rubyService: RubyApiService) { 
 
   }
 
@@ -18,5 +20,20 @@ export class ListaTaskPage implements OnInit {
 
   inserisciTask(){
     this.navCtrl.navigateRoot('/lista-task');
+  }
+
+  getListaTask() {
+    
+    this.rubyService.get_tasks(this.globalService.currentPatient.id).subscribe(
+      data => {
+        this.response = data   
+      },
+      error => {
+        console.log(error);
+      },
+      () => {
+       
+      }
+    );
   }
 }
