b651f153850b511c8e7616217c1803ac668e5d2d
nelloflorido <anielloflorido@gmail.com>
2020-01-13 10:15:22 +0100

    modifiche ion range

--- src/app/genericPackage/page/paziente/dettagli-Task/tab-dettagli/tab-dettagli.page.ts
+++ src/app/genericPackage/page/paziente/dettagli-Task/tab-dettagli/tab-dettagli.page.ts
@@ -7,10 +7,24 @@ import { GlobalService } from 'src/app/service/global.service';
   styleUrls: ['./tab-dettagli.page.scss'],
 })
 export class TabDettagliPage implements OnInit {
+num: number;
 
   constructor(public global:GlobalService) { }
 
   ngOnInit() {
+    this.global.currentTask.autonomy;
+    
+    //35,65,100
+    if(this.global.currentTask.autonomy==1){
+      this.num=35;
+    }
+    else if(this.global.currentTask.autonomy==2){
+      this.num=65;
+    }
+    else if(this.global.currentTask.autonomy==3){
+      this.num=100;
+    }
+
   }
 
 }
